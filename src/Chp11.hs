7195a33ec415858ae7fde6f859c536bbc8d8d7a3
James Yang <jamesyang124@gmail.com>
2017-07-29 22:33:38 +0800

    FIX. add dependency for directory, bytestring, random

--- src/Chp11.hs
+++ src/Chp11.hs
@@ -2,5 +2,8 @@
 
 {--
 
+Many times the box analogy is used to help you get some intuition for how functors work, and later, we'll probably use the same analogy for applicative functors and monads.
+
+A more correct term for what a functor is would be "computational context". The context might be that the computation can have a value or it might have failed (Maybe and Either a) or that there might be more values (lists), stuff like that.
 
 --}
