308fae085a2e27a7945562186a1510ff4bf35fe4
James Yang <jamesyang124@gmail.com>
2017-07-29 22:26:42 +0800

    UPD. import to Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,15 @@ import Chp4
 import Chp5
 import Chp6
 import Chp7
+import Chp81
+import Chp82
+import Chp83
+import Chp84
+import Chp91
+import Chp92
+import Chp93
+import Chp94
+import Chp95
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
