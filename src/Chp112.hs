b2abf87e6c3b91bf003d5e2f25cd476f2bd89c1a
James Yang <james_yang@htc.com>
2017-08-26 17:27:46 +0800

    ADD. newtype keyword

--- src/Chp112.hs
+++ src/Chp112.hs
@@ -1,5 +1,7 @@
 {-- The newtype keyword
 
+The value constructor of a newtype must have exactly one field, and only one value constructor
+
 The newtype keyword in Haskell is made exactly for these cases when we want to just take one type and wrap it in something to present it as another type. In the actual libraries,
 
 ZipList a is defined like this:
@@ -80,5 +82,73 @@ ghci> getPair $ fmap (*100) (Pair (2,3))
 
 ghci> getPair $ fmap reverse (Pair ("london calling", 3))
 ("gnillac nodnol",3)
+
+--}
+
+{--
+
+Haskell can represent the values of types defined with newtype just like the original ones, only it has to keep in mind that the their types are now distinct.
+
+This fact means that not only is newtype faster, it's also lazier. Let's take a look at what this means.
+
+The undefined value in Haskell represents an erronous computation. If we try to evaluate it (that is, force Haskell to actually compute it) by printing it to the terminal, Haskell will throw an exception
+
+However, if we make a list that has some undefined values in it but request only the head of the list, which is not undefined, everything will go smoothly because Haskell doesn't really need to evaluate any other elements in a list if we only want to see what the first element is:
+
+head [3,4,5,undefined,2,undefined]
+--}
+
+{--
+
+Let's make a function that pattern matches on a CoolBool and returns the value "hello" regardless of whether the Bool inside the CoolBool was True or False:
+
+helloMe :: CoolBool -> String
+helloMe (CoolBool _) = "hello"
+
+ghci> helloMe undefined
+"*** Exception: Prelude.undefined
+
+Types defined with the data keyword can have "multiple value constructors" (even though CoolBool only has one).
+
+So in order to see if the value given to our function conforms to the (CoolBool _) pattern, Haskell has to evaluate the value just enough to see which value constructor was used when we made the value.
+
+And when we try to evaluate an undefined value, even a little, an exception is thrown.
+
+newtype CoolBool = CoolBool { getCoolBool :: Bool }
+
+ghci> helloMe undefined
+"hello"
+
+when we use newtype, Haskell can internally represent the values of the new type in the same way as the original values.
+
+It doesn't have to add another box around them, it just has to be aware of the values being of different types.
+
+And because Haskell knows that types made with the newtype keyword can only have one constructor, it doesn't have to evaluate the value passed to the function to make sure that it conforms to the (CoolBool _) pattern because newtype types can only have one possible value constructor and one field!
+
+--}
+
+{-- type vs. newtype vs. data
+
+The type keyword is for making type synonyms.
+
+type IntList = [Int]
+
+All this does is to allow us to refer to the [Int] type as IntList.
+
+They can be used interchangeably. We don't get an IntList value constructor or anything like that.
+
+The newtype keyword is for taking existing types and wrapping them in new types, mostly so that it's easier to make them instances of certain type classes.
+
+newtype CharList = CharList { getCharList :: [Char] }
+
+We can't use ++ to put together a CharList and a list of type [Char]. We can't even use ++ to put together two CharLists, because ++ works only on lists and the CharList type isn't a list, even though it could be said that it contains one. We can, however, convert two CharLists to lists, ++ them and then convert that back to a CharList.
+
+When we use record syntax in our newtype declarations, we get functions for converting between the new type and the original type: namely the value constructor of our newtype and the function for extracting the value in its field.
+
+The new type also isn't automatically made an instance of the type classes that the original type belongs to, so we have to derive or manually write them.
+
+In practice, you can think of newtype declarations as data declarations that can only have one constructor and one field.
+
+If you catch yourself writing such a data declaration, consider using newtype.
 
 --}
---
+++ src/Chp113.hs
@@ -0,0 +1,6 @@
+{-- Monoid
+
+A monoid is when you have an associative binary function and a value which acts as an identity with respect to that function. 
+
+
+--}
